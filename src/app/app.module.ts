25c93f93d2cc14adf845fc20254b15b3c46d862e
Dmitry Nehaychik <4dmitr@gmail.com>
2017-04-03 18:03:43 +0300

    refactor(auth): rename dummy provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,7 +49,7 @@ import {
 } from './menu-test/menu-test.component';
 import { NgaUserTestComponent } from './user-test/user-test.component';
 import { NgaAuthModule } from '../framework/auth';
-import { NgaDummyProviderService } from '../framework/auth';
+import { NgaDummyAuthProvider } from '../framework/auth';
 
 import { routes } from './app.routes';
 import { menuItems } from './menu-test/menu-items';
@@ -102,7 +102,7 @@ const NGA_TEST_COMPONENTS = [
       providers: {
 
         email: {
-          service: NgaDummyProviderService,
+          service: NgaDummyAuthProvider,
           config: {
             alwaysFail: true,
             delay: 1000,
